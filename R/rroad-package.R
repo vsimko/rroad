242165adb577fc54dd29719406e03a33afdbad65
vsimko <viliam.simko@gmail.com>
2016-02-05 18:32:04 +0100

    precomputed IRI coeficients as a lazily evaluated promises (this can be done better in future)

--- R/rroad-package.R
+++ R/rroad-package.R
@@ -1,19 +1,21 @@
 .onAttach <- function(libname, pkgname) {
+
   # just to show a startup message
   message <- paste("rroad", utils::packageVersion("rroad"), "loaded.")
   packageStartupMessage(message, appendLF = TRUE)
+
+  # lazily evaluated promise (precomputed coeficients 100mm segments)
+  delayedAssign("IRI_COEF_100", list(
+    dx = 0.1,
+    st = matrix(nrow = 4, ncol = 4),
+    pr = c(0,0,0,0)
+  ), assign.env = as.environment("package:rroad"))
+
+  # lazily evaluated promise (precomputed coeficients 250mm segments)
+  delayedAssign("IRI_COEF_250", list(
+    dx = 0.25,
+    st = matrix(nrow = 4, ncol = 4),
+    pr = c(0,0,0,0)
+  ), assign.env = as.environment("package:rroad"))
+
 }
-
-#' Coeficients for computing IRI for 100mm segments
-iri_coef_100mm <- list(
-  dx = 0.1,
-  st = matrix(nrow = 4, ncol = 4),
-  pr = c(0,0,0,0)
-)
-
-#' Coeficients for computing IRI for 250mm segments
-iri_coef_250mm <- list(
-  dx = 0.25,
-  st = matrix(nrow = 4, ncol = 4),
-  pr = c(0,0,0,0)
-)
