245e0e65dd8659df8918cb3a2ae05fb972ceecef
laubis_koi <laubis@fzi.de>
2016-02-05 17:15:05 +0100

    created function declaration

--- R/iri.R
+++ R/iri.R
@@ -1,11 +1,15 @@
-#' Computes the IRI index from time series
+# This file provides a function for calculation the international roughness index (IRI) given a road profile.
+
+#' Computes the IRI for fixed length segments (e.g. 100 m segments) given a road profile
 #'
-#' @param ts Input time series
-#' @references some paper
+#' @param profile Road profile (as numeric vector) whose IRI is to be calculated.
+#' @param sample.interval Distance (in mm) between two samples of the given profile.
+#' @param segment.length: Distance (in m) for which the IRI is to be calculated. Default is 100 m.
+#' @return Calculated IRI (as numeric vector) of the given profile.
 #' @examples
-#' ts1 <- rnorm(10)
-#' iri <- ComputeIriIndex(ts)
+#' iri <- CalculateIRI(profile, 10)
 #' @export
-ComputeIriIndex <- function(ts) {
-  # here be dragons ...
+CalculateIRI <- function(profile, sample.interval, segment.length = 100) {
+  #
+  return(profile)
 }
