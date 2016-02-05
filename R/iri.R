147115f39dcb052d7a0012652a75a8a68180605b
Viliam Simko <viliam.simko@gmail.com>
2016-02-05 16:40:59 +0100

    added skeleton for the iri computation

---
+++ R/iri.R
@@ -0,0 +1,11 @@
+#' Computes the IRI index from time series
+#'
+#' @param ts Input time series
+#' @references some paper
+#' @examples
+#' ts1 <- rnorm(10)
+#' iri <- ComputeIriIndex(ts)
+#' @export
+ComputeIriIndex <- function(ts) {
+  # here be dragons ...
+}
