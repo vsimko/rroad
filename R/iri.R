a47727a4db5dd0a0eca114fde9789d7d6f3963a5
kelaub <info@kevin-laubis.de>
2016-02-08 19:15:27 +0100

    first draft of CalculateIRI implemented (resulting iris are not correct, yet)

--- R/iri.R
+++ R/iri.R
@@ -13,7 +13,7 @@ CalculateIRI <- function(profile, iri_coef, segment.length = 100) {
   # initialize costants
   DX <- iri_coef$dx  # sample interval (m)
   K <- as.integer(max(2, (0.25 / DX + 0.5) + 1))  # number of profile points used to compute mvg avg slope input (window)
-  BL <- (k - 1) * DX  # baselength
+  BL <- (K - 1) * DX  # baselength
   ST <- iri_coef$st  # coefficients of the iri equations (state transition)
   PR <- iri_coef$pr  # coefficients of the iri equations
 
@@ -22,6 +22,9 @@ CalculateIRI <- function(profile, iri_coef, segment.length = 100) {
   # num_segments <- length(profile) / num_samples_per_segment
   profile_segments <- split(profile, ceiling(seq_along(profile)/num_samples_per_segment))
 
+  # vector for collecting return value
+  iri <- numeric()
+
   # loop trough segments and calculate avg iri per segment
   for (profile_segment in profile_segments) {
     # initialize variables
@@ -29,29 +32,48 @@ CalculateIRI <- function(profile, iri_coef, segment.length = 100) {
     y[K] <- profile[K]  # elevation 11 m from start
     y[1] <- profile[1]  # elevation at beginning
     z_last <- list()  # vehicle variables (1 to 4) containing values from former profile point
-    z_last[1] <- (y(k) - y(1)) / 11 # TODO
+    z_last[1] <- (y[[K]] - y[[1]]) / 11
     z_last[2] <- 0
     z_last[3] <- z_last[1]
     z_last[4] <- z_last[2]
     rs <- 0  # rectified slope / accumulated slope
     iri_df <- data.frame()  # df for colleteing return values
+    ix <- 1  # index within sliding window
 
     # calculate avg IRI per segment; loop through profile points in segment
     for (i in length(profile_segment)) {
-      # loop trough slope calculation window (filling window)
-      for (ix in 1:K-1) {
-        y[K] <- profile[ix]
+      # filling window; loop trough slope calculation window
+      y[K] <- profile_segment[ix]
+      ix <- ix + 1
+      while (ix < K) {
+        y[ix] <- y[K]
+        y[K] <- profile_segment[ix]
+        ix <- ix + 1
       }
+
       # compute slope input
-      # TODO
+      yp <- (y[[K]] - y[[1]]) / BL
+      for (j in 2:K) {
+        y[j-1] <- y[j]
+      }
 
       # simulate vehicle response for determining accumulated rs
-      # TODO
-      rs <- rs + abs()
+      z <- list()  # vehicle variables (1 to 4)
+
+      for (j in 1:4) {
+        z[j] <- PR[[j]] * yp
+        for (jj in 1:4) {
+          z[j] <- z[[j]] + ST[[j,jj]] * z_last[[jj]]
+        }
       }
+      rs <- rs + abs(z[[1]]- z[[3]])
+
+      # store vehicle variables (1 to 4) for next profile input
+      z_last <- z
+    }
 
     # determine avg rs by dividing by number of samples per segment and attach to result vector
-    segm_iri <- rs / i
+    segm_iri <- rs / length(profile_segment)
     iri <- c(iri, segm_iri)
   }
 
--- R/rroad-package.R
+++ R/rroad-package.R
@@ -7,15 +7,21 @@
   # lazily evaluated promise (precomputed coeficients 100mm segments)
   delayedAssign("IRI_COEF_100", list(
     dx = 0.1,
-    st = matrix(nrow = 4, ncol = 4),
-    pr = c(0,0,0,0)
+    st = t(matrix(nrow = 4, ncol = 4,
+                  c(.9994014, 4.442351E-03, 2.188854E-04, 5.72179E-05,
+                    -.2570548, .975036, 7.966216E-03, 2.458427E-02,
+                    3.960378E-03, 3.814527E-04, .9548048, 4.055587E-03,
+                    1.687312, .1638951, -19.34264, .7948701))),
+    pr = c(3.793992E-04, .2490886, 4.123478E-02, 17.65532)
   ), assign.env = as.environment("package:rroad"))
 
-  # lazily evaluated promise (precomputed coeficients 250mm segments)
-  delayedAssign("IRI_COEF_250", list(
-    dx = 0.25,
-    st = matrix(nrow = 4, ncol = 4),
-    pr = c(0,0,0,0)
-  ), assign.env = as.environment("package:rroad"))
+#   # lazily evaluated promise (precomputed coeficients 250mm segments)
+#   delayedAssign("IRI_COEF_250", list(
+#     dx = 0.25,
+#     st = matrix(nrow = 4, ncol = 4),  # TODO
+#     pr = c(0.005476107, 1.388776, 0.2275968, 35.79262)
+#   ), assign.env = as.environment("package:rroad"))
 
 }
+
+
