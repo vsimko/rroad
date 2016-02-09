f58879936ebb164af4fcb4f0d919c57bf6a08e7f
kelaub <info@kevin-laubis.de>
2016-02-09 13:35:55 +0100

    fixed error in within segment-loop; avoid reading out of segment; changed lists to vectors

--- R/iri.R
+++ R/iri.R
@@ -12,13 +12,14 @@
 CalculateIRI <- function(profile, iri_coef, segment.length = 100) {
   # initialize costants
   DX <- iri_coef$dx  # sample interval (m)
-  K <- as.integer(max(2, (0.25 / DX + 0.5) + 1))  # number of profile points used to compute mvg avg slope input (window)
+  K <- max(2, as.integer(.5 + .25 / DX) + 1)  # number of profile points used to compute mvg avg slope input (window)
   BL <- (K - 1) * DX  # baselength
   ST <- iri_coef$st  # coefficients of the iri equations (state transition)
   PR <- iri_coef$pr  # coefficients of the iri equations
 
   # split profile into segments (e.g. per 100m segment)
   num_samples_per_segment <- segment.length / DX
+
   # num_segments <- length(profile) / num_samples_per_segment
   profile_segments <- split(profile, ceiling(seq_along(profile)/num_samples_per_segment))
 
@@ -28,22 +29,30 @@ CalculateIRI <- function(profile, iri_coef, segment.length = 100) {
   # loop trough segments and calculate avg iri per segment
   for (profile_segment in profile_segments) {
     # initialize variables
-    y <- list()  # sliding window of profil elevations for calculating mvg avg slope (buffer of length K)
+
+    # sliding window of profil elevations for calculating mvg avg slope (buffer of length K)
+    y <- rep(0, 26)
     y[K] <- profile[K]  # elevation 11 m from start
     y[1] <- profile[1]  # elevation at beginning
-    z_last <- list()  # vehicle variables (1 to 4) containing values from former profile point
-    z_last[1] <- (y[[K]] - y[[1]]) / 11
+
+    # vehicle variables (1 to 4) containing values from former profil epoint
+    z_last <- vector()
+    z_last[1] <- (y[K] - y[1]) / 11
     z_last[2] <- 0
     z_last[3] <- z_last[1]
     z_last[4] <- z_last[2]
+
     rs <- 0  # rectified slope / accumulated slope
     iri_df <- data.frame()  # df for colleteing return values
     ix <- 1  # index within sliding window
 
     # calculate avg IRI per segment; loop through profile points in segment
-    for (i in length(profile_segment)) {
+    for (i in seq_along(profile_segment)) {
       # filling window; loop trough slope calculation window
-      y[K] <- profile_segment[ix]
+      if (length(profile_segment) >= ix) { # added conditon for not reading out of segment
+        y[K] <- profile_segment[ix]
+      }
+
       ix <- ix + 1
       while (ix < K) {
         y[ix] <- y[K]
@@ -52,21 +61,20 @@ CalculateIRI <- function(profile, iri_coef, segment.length = 100) {
       }
 
       # compute slope input
-      yp <- (y[[K]] - y[[1]]) / BL
+      yp <- (y[K] - y[1]) / BL
       for (j in 2:K) {
         y[j-1] <- y[j]
       }
 
       # simulate vehicle response for determining accumulated rs
-      z <- list()  # vehicle variables (1 to 4)
-
+      z <- vector()  # vehicle variables (1 to 4)
       for (j in 1:4) {
         z[j] <- PR[[j]] * yp
         for (jj in 1:4) {
-          z[j] <- z[[j]] + ST[[j,jj]] * z_last[[jj]]
+          z[j] <- z[j] + ST[[j,jj]] * z_last[jj]
         }
       }
-      rs <- rs + abs(z[[1]]- z[[3]])
+      rs <- rs + abs(z[1]- z[3])
 
       # store vehicle variables (1 to 4) for next profile input
       z_last <- z
--- R/rroad-package.R
+++ R/rroad-package.R
@@ -7,11 +7,11 @@
   # lazily evaluated promise (precomputed coeficients 100mm segments)
   delayedAssign("IRI_COEF_100", list(
     dx = 0.1,
-    st = t(matrix(nrow = 4, ncol = 4,
+    st = matrix(nrow = 4, ncol = 4, byrow = TRUE,
                   c(.9994014, 4.442351E-03, 2.188854E-04, 5.72179E-05,
                     -.2570548, .975036, 7.966216E-03, 2.458427E-02,
                     3.960378E-03, 3.814527E-04, .9548048, 4.055587E-03,
-                    1.687312, .1638951, -19.34264, .7948701))),
+                    1.687312, .1638951, -19.34264, .7948701)),
     pr = c(3.793992E-04, .2490886, 4.123478E-02, 17.65532)
   ), assign.env = as.environment("package:rroad"))
 
